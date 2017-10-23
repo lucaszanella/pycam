a21dc255510c9369e98ee6d03138c5321c1f0ed8
Lucas Zanella <me@lucaszanella.com>
2017-10-22 23:54:21 -0200

    added media support and profiles support

--- pycam.py
+++ pycam.py
@@ -2,6 +2,7 @@ import sys
 from custom_transport import *
 
 sys.path.insert(0, '/home/lz/Coding/python-onvif-zeep/onvif')
+sys.path.insert(0, '/home/lz/Coding/python-rtsp-client')
 
 from client import *
 
@@ -29,8 +30,20 @@ for camera in cameras:
     imaging_url = ''
     mycam = ONVIFCamera(camera['ip'], camera['onvif'], camera['username'], camera['password'], wsdl, transport=SocksTransport)
     resp = mycam.devicemgmt.GetCapabilities()
+
     if resp["Imaging"]:
         imaging_url = resp["Imaging"]["XAddr"]
-    resp = mycam.devicemgmt.GetServiceCapabilities()
-    print(resp)
+    if resp["Media"]:
+        print(resp["Media"])
+        media_service = mycam.create_media_service()
+        profiles = media_service.GetProfiles()
+        #print(profiles)
+        # Use the first profile and Profiles have at least one
+        token = profiles[0].token
+        params = mycam.devicemgmt.create_type('GetSystemUris')
+        resp = mycam.media.GetStreamUri({'StreamSetup':{'Stream':'RTP-Unicast', 'Transport': {'Protocol': 'RTSP'}}, 'ProfileToken':token})
+        print(resp)
+    #print(resp)
+    #resp = mycam.media.GetStreamUri()
+    #print(resp)
 

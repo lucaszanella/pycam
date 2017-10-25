0fc4a58053ea7f679c071ec78ed70fd5ae73a6ca
Lucas Zanella <me@lucaszanella.com>
2017-10-25 00:11:43 -0200

    made camera be an object

--- pycam.py
+++ pycam.py
@@ -7,9 +7,7 @@ sys.path.insert(0, '/home/lz/Coding/python-onvif-zeep/onvif')
 sys.path.insert(0, '/home/lz/Coding/python-rtsp-client')
 
 from client import *
-
-def log(a):
-    print(a)
+from rtsp import RTSPClient
 
 def load_cameras():
     import json
@@ -29,41 +27,66 @@ proxies = {
     'https': 'socks5://' + user + ':' + password + '@' + host + ':' + str(port)
 }
 #----------------------------
-
 SocksTransport = CustomTransport(proxies)
 thread_list = []
 
+class Camera():
+    def __init__(self, id=None, ip=None, onvif=None, rtsp=None, username=None, password=None):
+        self.id = id
+        self.ip = ip
+        self.onvif = onvif
+        self.rtsp = rtsp
+        self.username = username
+        self.password = password
+        self.rtsp_uri = None
+    def log(self, info):
+        print('Camera ' + str(self.id) + ': ' + info)    
+
+
 def load_camera_information(camera):
-    id = 'Camera ' + str(camera['id'])
-    log(id + ': ' + 'loading information...')
-    mycam = ONVIFCamera(camera['ip'], 
-                        camera['onvif_port'],
-                        camera['username'], 
-                        camera['password'],
-                        camera['wsdl'], 
+    camera.log('loading information...')
+    camera.log('IP: ' + camera.ip + ', ONVIF: ' + str(camera.onvif))
+    mycam = ONVIFCamera(camera.ip, 
+                        camera.onvif,
+                        camera.username, 
+                        camera.password,
+                        wsdl, 
                         transport=SocksTransport)
-    log(id + ': ' + 'getting capabilities...')
+    camera.log('getting capabilities...')
     resp = mycam.devicemgmt.GetCapabilities()
     if resp["Imaging"]:
-        log(id + ': ' + 'supports imaging services')
+        camera.log('supports imaging services')
         imaging_url = resp["Imaging"]["XAddr"]
     if resp["Media"]:
-        log(id + ': ' + 'supports media services')
-        log(id + ': ' + 'querying media services...')
+        camera.log('supports media services')
+        camera.log('querying media services...')
         media_service = mycam.create_media_service()
-        log(id + ': ' + 'querying profiles...')
+        camera.log('querying profiles...')
         profiles = media_service.GetProfiles()
         # Use the first profile and Profiles have at least one
         token = profiles[0].token
-        log(id + ': ' + 'getting system uri...')
+        camera.log('getting system uri...')
         params = mycam.devicemgmt.create_type('GetSystemUris')
         resp = mycam.media.GetStreamUri({'StreamSetup':{'Stream':'RTP-Unicast', 'Transport': {'Protocol': 'RTSP'}}, 'ProfileToken':token})
-        camera['camera_information'] = {}
-        camera['camera_information']['rtsp'] = resp["XAddr"]
+        #print(resp)
+        camera.rtsp_uri = resp["Uri"]
     return camera
 
-def negotiate_stream_parameters(camera):
-    print(camera['camera_information'])
+def rtsp_connect(camera):
+    url = camera.rtsp_uri#.replace('554\/11', '10554')
+    camera.log('opening RTSP connection to url ' + url + ' ...')
+    myrtsp = RTSPClient(url=url,callback=print)
+    try:
+        myrtsp.do_describe()
+        while myrtsp.state != 'describe':
+            time.sleep(0.1)
+        myrtsp.do_setup(rtsp.track_id_str)
+        while myrtsp.state != 'setup':
+            time.sleep(0.1)
+        #Open socket to capture frames here
+        myrtsp.do_play(rtsp.cur_range, rtsp.cur_scale)
+    except:
+        myrtsp.do_teardown()
     return camera
 
 def begin_stream(camera):
@@ -72,14 +95,24 @@ def begin_stream(camera):
 #take_until(cancel_launch).\
 #delay_with_selector(lambda s: Observable.timer(2**s*500))
 
+example_of_camera = load_cameras()[1]
+example_of_camera = Camera(
+                            id = example_of_camera['id'],
+                            ip = example_of_camera['ip'],
+                            onvif = example_of_camera['onvif'],
+                            rtsp = example_of_camera['rtsp'],
+                            username = example_of_camera['username'],
+                            password = example_of_camera['password']
+                            )
+
 launch_camera = Observable.\
-        just(load_cameras()[0]).\
+        just(example_of_camera).\
 		do_action(load_camera_information).\
-		do_action(negotiate_stream_parameters).\
+		do_action(rtsp_connect).\
 		do_action(begin_stream)
 		
 launch_camera.subscribe(
-            on_next=lambda s: print(s),
-			on_completed=lambda: print('done'),
-			on_error=lambda e: print(e)
-			)
+                        on_next=lambda s: print(s),
+                        on_completed=lambda: print('done'),
+                        on_error=lambda e: print(e)
+                        )

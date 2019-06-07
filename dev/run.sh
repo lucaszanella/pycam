a5174bb12643b8b0bd07b61fcf873fcd7688bc25
Lucas Zanela <me@lucaszanella.com>
2019-06-07 19:05:56 -0300

    Update run.sh

--- dev/run.sh
+++ dev/run.sh
@@ -3,4 +3,4 @@ USER_=$USER
 #Then it's just a matter of sending to github and rebuilding the docker with no cache.
 INSERT_LOCAL="-v /home/$USER_/python-rtsp-client:/home/deps/python-rtsp-client"
 #INSERT_LOCAL=
-xhost local:root && docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/$USER_/pycam:/home/project $(echo $INSERT_LOCAL) -e DISPLAY=unix$DISPLAY pycam && xhost -
+xhost local:root && sudo docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/$USER_/pycam:/home/project $(echo $INSERT_LOCAL) -e DISPLAY=unix$DISPLAY pycam && xhost -
